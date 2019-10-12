ae8aa530ae6baba6ae9f362106a83278de647e4b
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2019-10-12 14:00:12 -0400

    Updates

---
+++ precache-manifest.9d6c69a6aba8658c24546cd9b03a3c02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62462e4cc3b32fb7777c56fad3861885",
+    "url": "/darzyx/index.html"
+  },
+  {
+    "revision": "0be4daee1845c22483ac",
+    "url": "/darzyx/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f90d20691d9816d34fd3",
+    "url": "/darzyx/static/js/2.4e2eee84.chunk.js"
+  },
+  {
+    "revision": "0be4daee1845c22483ac",
+    "url": "/darzyx/static/js/main.4e645f24.chunk.js"
+  },
+  {
+    "revision": "e6bd407bb0b2c3c729fd",
+    "url": "/darzyx/static/js/runtime-main.7c5b4084.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/darzyx/static/media/logo.25bf045c.svg"
+  }
+]);
