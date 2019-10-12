c01819940c78ef017afe32e1c9f27c8abf555f8a
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2019-10-12 17:02:36 -0400

    Update / standardize formatting

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,42 +1,48 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => (
-  <section className='header' id='header'>
-    <nav className='navbar'>
-      <div className='nav-left'>
-        <a className='link' onClick={() => window.location.reload()}>
-          <span className='bra'>〈</span>
-            J.D.S.
-          <span className='ket'>〉</span>
+  <section className="header" id="header">
+    <nav className="navbar">
+      <div className="nav-left">
+        <a className="link" onClick={() => window.location.reload()}>
+          <span className="bra">〈</span>
+          J.D.S.
+          <span className="ket">〉</span>
         </a>
       </div>
-      <div className='nav-right'>
-        <a onClick={() => document
-          .querySelector('#projects-heading')
-          .scrollIntoView({behavior: 'smooth', block: 'start'})}
+      <div className="nav-right">
+        <a
+          onClick={() =>
+            document
+              .querySelector("#projects-heading")
+              .scrollIntoView({ behavior: "smooth", block: "start" })
+          }
         >
-          <span className='bra'>〈</span>
-            projects
-          <span className='ket'>〉</span>
+          <span className="bra">〈</span>
+          projects
+          <span className="ket">〉</span>
         </a>
       </div>
-      <div className='nav-right'>
-        <a onClick={() => document
-          .querySelector('#contact-heading')
-          .scrollIntoView({behavior: 'smooth', block: 'start'})}
+      <div className="nav-right">
+        <a
+          onClick={() =>
+            document
+              .querySelector("#contact-heading")
+              .scrollIntoView({ behavior: "smooth", block: "start" })
+          }
         >
-          <span className='bra'>〈</span>
-            contact
-          <span className='ket'>〉</span>
+          <span className="bra">〈</span>
+          contact
+          <span className="ket">〉</span>
         </a>
       </div>
     </nav>
-    <div className='content'>
-      <h1 className='title'>Jose Dario Sanchez</h1>
-      <h3 className='subtitle'>Frontend Developer</h3>
+    <div className="content">
+      <h1 className="title">Jose Dario Sanchez</h1>
+      <h3 className="subtitle">Frontend Developer</h3>
     </div>
-    <div className='triangle'></div>
+    <div className="triangle"></div>
   </section>
-)
+);
 
-export default Header
+export default Header;
