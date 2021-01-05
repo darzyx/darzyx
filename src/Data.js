0b19c62a6b44ce10bb19b0a5a02900da9de8f5e0
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2021-01-05 18:11:12 -0500

    Reorder projects

--- src/Data.js
+++ src/Data.js
@@ -1,5 +1,21 @@
 export const ProjectsData = [
   {
+    name: "HiTech Lowlife",
+    description:
+      "Mock-up. Desktop-based, 2D platformer, pixel art video game level written in JavaScript. Features open source, web optimized pixel art and audio.",
+    tools: [
+      { name: "Phaser", link: "http://phaser.io/" },
+      {
+        name: "Photoshop",
+        link: "http://www.adobe.com/products/photoshop.html",
+      },
+      { name: "Tiled", link: "http://www.mapeditor.org/" },
+    ],
+    demoLink: "https://darzyx.github.io/HiTechLowlife/",
+    sourceLink: "https://github.com/darzyx/HiTechLowlife",
+    image: require("./Media/Images/hitechlowlife.png"),
+  },
+  {
     name: "AV Happenings",
     description:
       "Prototype open-source, single-page progressive web app for discovering, sharing, and talking about community events local to the Antelope Valley CA, USA. Under active development. Features client side routing, mobile installation, HTTPS, user login/authentication, backend, and excellent performance.",
@@ -7,11 +23,23 @@ export const ProjectsData = [
       { name: "React", link: "https://reactjs.org/" },
       { name: "Firebase", link: "https://firebase.google.com/" },
       { name: "Semantic UI", link: "https://semantic-ui.com/" },
-      { name: "React Router", link: "https://reacttraining.com/react-router/" }
+      { name: "React Router", link: "https://reacttraining.com/react-router/" },
     ],
     demoLink: "https://www.avhappenings.com/",
     sourceLink: "https://github.com/darzyx/av-happenings",
-    image: require("./Media/Images/avhappenings.png")
+    image: require("./Media/Images/avhappenings.png"),
+  },
+  {
+    name: "Faux Blockchain Services",
+    description:
+      "A website design/mockup for an imaginary blockchain services company. Features original design and landing page animation.",
+    tools: [
+      { name: "React", link: "https://reactjs.org/" },
+      { name: "Styled Components", link: "https://www.styled-components.com/" },
+    ],
+    demoLink: "https://darzyx.github.io/faux-blockchain-services/",
+    sourceLink: "https://github.com/darzyx/faux-blockchain-services",
+    image: require("./Media/Images/fauxblockchain.png"),
   },
   {
     name: "Portfolio Website",
@@ -22,12 +50,12 @@ export const ProjectsData = [
       { name: "SASS", link: "http://sass-lang.com/" },
       {
         name: "Photoshop",
-        link: "http://www.adobe.com/products/photoshop.html"
-      }
+        link: "http://www.adobe.com/products/photoshop.html",
+      },
     ],
     demoLink: "https://www.darzyx.com/",
     sourceLink: "https://github.com/darzyx/darzyx",
-    image: require("./Media/Images/background-xs.jpg")
+    image: require("./Media/Images/background-xs.jpg"),
   },
   {
     name: "Near Earth Objects",
@@ -36,39 +64,11 @@ export const ProjectsData = [
     tools: [
       { name: "React", link: "https://reactjs.org/" },
       { name: "Redux", link: "https://redux.js.org/" },
-      { name: "Bootstrap", link: "https://getbootstrap.com/" }
+      { name: "Bootstrap", link: "https://getbootstrap.com/" },
     ],
     demoLink: "https://darzyx.github.io/near-earth-objects/",
     sourceLink: "https://github.com/darzyx/near-earth-objects",
-    image: require("./Media/Images/nearearthobjects.png")
-  },
-  {
-    name: "Faux Blockchain Services",
-    description:
-      "A website design/mockup for an imaginary blockchain services company. Features original design and landing page animation.",
-    tools: [
-      { name: "React", link: "https://reactjs.org/" },
-      { name: "Styled Components", link: "https://www.styled-components.com/" }
-    ],
-    demoLink: "https://darzyx.github.io/faux-blockchain-services/",
-    sourceLink: "https://github.com/darzyx/faux-blockchain-services",
-    image: require("./Media/Images/fauxblockchain.png")
-  },
-  {
-    name: "HiTech Lowlife",
-    description:
-      "Mock-up. Desktop-based, 2D platformer, pixel art video game level written in JavaScript. Features open source, web optimized pixel art and audio.",
-    tools: [
-      { name: "Phaser", link: "http://phaser.io/" },
-      {
-        name: "Photoshop",
-        link: "http://www.adobe.com/products/photoshop.html"
-      },
-      { name: "Tiled", link: "http://www.mapeditor.org/" }
-    ],
-    demoLink: "https://darzyx.github.io/HiTechLowlife/",
-    sourceLink: "https://github.com/darzyx/HiTechLowlife",
-    image: require("./Media/Images/hitechlowlife.png")
+    image: require("./Media/Images/nearearthobjects.png"),
   },
   {
     name: "RC Precision Grinding",
@@ -78,22 +78,22 @@ export const ProjectsData = [
       { name: "React", link: "https://reactjs.org/" },
       { name: "HTML5" },
       { name: "CSS3" },
-      { name: "Semantic UI", link: "https://semantic-ui.com/" }
+      { name: "Semantic UI", link: "https://semantic-ui.com/" },
     ],
     demoLink: "https://darzyx.github.io/rc-precision-grinding/",
     sourceLink: "https://github.com/darzyx/rc-precision-grinding",
-    image: require("./Media/Images/rcprecisiongrinding.png")
+    image: require("./Media/Images/rcprecisiongrinding.png"),
   },
   {
     name: "Physics Data Visualization",
     description:
       "Repository of undergraduate physics projects coded in Mathematica (Wolfram). Physics phenomena and events are modeled, animated, and their data is extracted and visualized. Demo requires Mathematica.",
     tools: [
-      { name: "Mathematica", link: "https://www.wolfram.com/mathematica/" }
+      { name: "Mathematica", link: "https://www.wolfram.com/mathematica/" },
     ],
     demoLink: "",
     sourceLink: "https://github.com/darzyx/Mathematica",
-    image: require("./Media/Images/mathematica.png")
+    image: require("./Media/Images/mathematica.png"),
   },
   {
     name: "Processing Sketches",
@@ -102,7 +102,7 @@ export const ProjectsData = [
     tools: [{ name: "Processing", link: "https://processing.org/" }],
     demoLink: "https://darzyx.github.io/Processing/",
     sourceLink: "https://github.com/darzyx/Processing",
-    image: require("./Media/Images/processing.png")
+    image: require("./Media/Images/processing.png"),
   },
   {
     name: "Everyst",
@@ -110,6 +110,6 @@ export const ProjectsData = [
       "Website quickly developed for a small non-profit gaming group with a pressing deadline. Features original design.",
     tools: [{ name: "HTML5" }, { name: "CSS3" }],
     demoLink: "https://everystwow.com/",
-    image: require("./Media/Images/everystwow.png")
-  }
+    image: require("./Media/Images/everystwow.png"),
+  },
 ];
