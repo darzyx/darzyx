ce13261660f108b9b4772bc0204ed99956893022
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2021-01-05 21:25:51 -0500

    Update skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -14,26 +14,28 @@ const Skills = () => (
     <div className="content">
       <div className="langs">
         <div className="left">
-          <span className="html-icon" />
-          <h5>HTML5</h5>
-          <h6>Accessibility</h6>
-          <h6>Usability</h6>
-        </div>
-        <div className="middle">
           <span className="js-icon" />
           <h5>JavaScript</h5>
-          <h6>ES6</h6>
-          <h6>React/JSX</h6>
+          <h6>React + Hooks</h6>
+          <h6>ES6, JSX</h6>
+          <h6>Reusable Components</h6>
           <h6>Redux</h6>
           <h6>Phaser</h6>
         </div>
-        <div className="right">
+        <div className="middle">
           <span className="css-icon" />
           <h5>CSS3</h5>
+          <h6>Styled Components</h6>
           <h6>Raw CSS</h6>
           <h6>SASS/SCSS</h6>
           <h6>Semantic UI</h6>
           <h6>Bootstrap</h6>
+        </div>
+        <div className="right">
+          <span className="html-icon" />
+          <h5>HTML5</h5>
+          <h6>Accessibility</h6>
+          <h6>Usability</h6>
         </div>
       </div>
       <div className="other">
@@ -49,15 +51,12 @@ const Skills = () => (
           <h5>Misc</h5>
           <h6>Responsive Design</h6>
           <h6>Basic UX/Design</h6>
-          <h6>Solidity</h6>
         </div>
         <div className="academic">
           <span className="academic-icon" />
-          <h5>University Education</h5>
-          <h6>Python</h6>
-          <h6>C/C++</h6>
-          <h6>Mathematica</h6>
-          <h6>Java</h6>
+          <h5>Currently Learning</h5>
+          <h6>Rust</h6>
+          <h6>Solidity</h6>
         </div>
       </div>
       <div className="download-container">
